71f54c7657d83ccea22ecccdaaae5642e8da077a
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2023-02-01 21:39:45 -0500

    made test files for us

---
+++ Group9_Store/alex.swift
@@ -0,0 +1,10 @@
+//
+//  alex.swift
+//  Group9_Store
+//
+//  Created by Alex Olechnowicz on 2023-02-01.
+//
+
+import Foundation
+
+//I'll do testing here
---
+++ Group9_Store/ameya.swift
@@ -0,0 +1,10 @@
+//
+//  ameya.swift
+//  Group9_Store
+//
+//  Created by Alex Olechnowicz on 2023-02-01.
+//
+
+import Foundation
+
+//You can do testing here
